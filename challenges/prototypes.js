812f5677b18d6092b27f47ea5c1d36344c0c9349
Ellen Weng <wengellen@gmail.com>
2019-03-01 09:55:08 -0800

    Finishes: Task 4: Classes and Stretch

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -29,7 +29,7 @@ CuboidMaker.prototype.volume = function(){
 */
 
 CuboidMaker.prototype.surfaceArea = function(){
-  return 2 * (this.length * this.width * this.height)
+  return 2 * ((this.length * this.width) + ( this.length  * this.height) + ( this.width * this.height))
 }
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
@@ -41,6 +41,6 @@ let cuboid = new CuboidMaker({length:4, width:5, height:5})
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.surfaceArea()); // 130 
 
 
